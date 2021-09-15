2650df317725101ebd508948d72800df8f22da6b
Jaira-Beletable <jayjbeletable@gmail.com>
2021-09-14 21:11:44 -0300

    salvando exercicio string e arrays

---
+++ string-arrays/exercicio javascript string e arrays.js
@@ -0,0 +1,34 @@
+//console.log ('ola mundo')
+
+//exercicio 1
+
+//let nomeDoUsuario = prompt ('qual seu nome') 
+//let email = prompt ('qual seu email ')
+//console.log (`o email` ,email ,`foi cadastrado com sucesso.´seja bem-vinda (o)`, nomeDoUsuario)
+
+//exercicio 2
+
+//let minhasComidasPreferidas = [ 'macarrao','hamburguer','batata cheddar e bacon','pizza','pudim'] 
+//console.log (minhasComidasPreferidas)
+//console.log ['essas sao minhas comidas favoritas'
+//'macarrao' ,
+//'hamburguer' ,
+//'batata cheddar e bacon',
+//'pizza',
+//'pudim']
+
+//let comidaPreferida = prompt ('qual sua comida preferida')
+//let minhasComidasPreferidas = [ 'macarrao','hamburguer','batata cheddar e bacon','pizza','pudim']
+ // console.log (minhasComidasPreferidas.push(comidaPreferida))
+
+//exercicio 3
+
+//let variavel = [tomar cafe,caminhar,ler]
+//let tarefa = prompt('qual sua primeira tarefa do dia')
+//let tarefa2 = prompt('qual segunda tarefa do dia')
+//let tarefa3 = prompt('qual sua terceira tarefa do dia')
+//console.log (variavel)
+//let tarefaRealizada = prompt('qual tarefa voce ja realizou')
+//let variavel.splice(0,1)
+//console.log (variavel)
+
