b110215757402c9234445fbd8b031c1fd7335e03
Jaira-Beletable <jayjbeletable@gmail.com>
2021-09-18 12:02:54 -0300

    salvando projeto quinzena1

---
+++ lista-logica/index.js
@@ -0,0 +1,48 @@
+//console.log ("ola mundo")
+
+//exercicio 1
+
+//const altura = 12
+//const largura = 10
+//cosnt areaDoRentangulo = 12 * 10
+//console.log(areaDoRentangulo)
+
+//exercicio 2
+
+//const anoAtual = prompt("que ano estamos")
+//const anoDeNascimento = prompt("qual seu ano de nascimento")
+//const idadeDoUsuario = anoAtual - anoDeNascimento
+//console.log(idadeDoUsuario)
+
+
+//exercicio 3
+
+//const peso = prompt("qual seu peso")
+//const altura = prompt("qual sua altura")
+//const imc = peso / (altura * altura)
+//console.log(imc)
+
+//exercicio 4
+
+//const nome = prompt(" qual seu nome")
+//const idade = number(prompt("qual sua idade")
+//const email = prompt("qual seu email")
+//console.log(`meu nome é ${nome}, tenho ${idade} anos ,e o meu email é ${email}`)
+ 
+ //exercicio 5
+
+ //const cor1 = "azul"
+ //const cor2 = "preto"
+ //const cor3 = "branco"
+ //const cores = ["azul","preto","branco"]
+ //console.log(cores)
+
+ //exercicio 6
+
+//const string = prompt("diga uma frase")
+//const fraseMaiuscula = string.toUppercase()
+//console.log(fraseMaiuscula)
+ 
+ 
+ 
+ 
---
+++ lista-logica/projeto-quinzena1.js
@@ -0,0 +1,96 @@
+//console.log ("ola mundo")
+
+//exercicio 1
+
+//const altura = 12
+const largura = 10
+const areaDoRetangulo = 12 * 10
+console.log(areaDoRetangulo)
+
+//exercicio 2
+
+//const anoAtual = prompt("que ano estamos")
+//const anoDeNascimento = prompt("qual seu ano de nascimento")
+//const idadeDoUsuario = anoAtual - anoDeNascimento
+//console.log(idadeDoUsuario)
+
+
+//exercicio 3
+
+//const peso = prompt("qual seu peso")
+//const altura = prompt("qual sua altura")
+//const imc = peso / (altura * altura)
+//console.log(imc)
+
+//exercicio 4
+
+//const nome = prompt(" qual seu nome")
+//const idade = number(prompt("qual sua idade")
+//const email = prompt("qual seu email")
+//console.log(`meu nome é ${nome}, tenho ${idade} anos ,e o meu email é ${email}`)
+ 
+ //exercicio 5
+
+ //const cor1 = "azul"
+ //const cor2 = "preto"
+ //const cor3 = "branco"
+ //const cores = ["azul","preto","branco"]
+ //console.log(cores)
+
+ //exercicio 6
+
+//const string = prompt("diga uma frase")
+//const fraseMaiuscula = string.toUpperCase()
+//console.log(fraseMaiuscula)
+ 
+ //excercicio 7
+
+//const valorDoIngresso = prompt("qual o valor do ingresso")
+//const custoDoEspetaculo = 3000
+//const resultado = (valorDoIngresso) / 3000 
+//console.log(resultado)
+ 
+//exercicio 8
+
+//const string1 = prompt("qual seu nome")
+//const string2  = prompt("qual sua idade")
+ const comparacaoDeTamanho = (string1 || string2)
+ const tamanhoString1 = (string1.length)
+ const tamanhoString2 = (string2.length)
+ console.log(comparacaoDeTamanho)
+
+ exercicio 9
+
+//const string3 = prompt("qual sua altura")
+//const string4 = prompt("qual seu numero favorito")
+//const saoIguaisNoTamanho = (string3.toLowerCase ! string4.toLowerCase)
+//console.log(saoIguaisNoTamanho)
+
+//exercicio 10
+
+//function checarRenovacaoRg(){
+  //  const anoAtual = prompt("qual ano atual")
+  //  const nascimento = ptompt("sua data de nascimento")
+// const anoDeEmissao = prompt("ano de emissao do Rg")
+ // const idade = anoAtual - anoDeNascimento
+ //  const anoRg = anoAtual - anoDeEmissao
+ //  console.log ((idade<=20 && anoRg>=5) || (idade> 20 && idade< 50 && anoRg>= 10) || ( idade>= 50 && anoRg >= 15))
+ //  }
+  //  checarRenovacaoRg()
+  // exercicio 11
+
+  // const checagemAnoBissexto = prompt("digite um ano")
+  // const bissexto1 = (checagemAnoBissexto % 400) == 0
+ //const bissexto2 = ((checagemAnoBissexto % 4 == 0) && (checagemAnoBissexto % 100 ! = 0 ))
+// const anoNaoBissexto = checagemAnoBissexto && bissexto1 && bissexto2
+ // console.log(anoNaoBissexto || bissexto1 || bissexto1)
+
+//exercicio 12
+
+//function estudanteLabenu(){
+  //  const suaIdade = prompt("voce tem mais de 18 anos")
+  //  const escolaridade = prompt("voce possui ensino medio completo")
+  //  const disponibilidade = prompt("voce possui disponibilidade de horario")
+ //const resultadoDaInscricao = ((suaIdade && escolaridade && disponibilidade))
+ //console.log(resultadoDaInscricao)}
+ //estudadeLabenu()
