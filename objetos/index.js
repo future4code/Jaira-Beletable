aa40f7042467bf694cbcd55b5996d9149435f650
Jaira-Beletable <jayjbeletable@gmail.com>
2021-09-21 15:19:28 -0300

    salvando exercicio objetos

---
+++ objetos/index.js
@@ -0,0 +1,61 @@
+//objetos aula07
+
+//console.log("ola mundo")
+
+//exercicio 1
+
+//const pessoa = {
+//nome: "mariana" ,
+  //apelido:["mari" , "ma" , "olhuda"]}
+
+ // function comoMeChamo(pessoa) {
+ //    [ ...pessoa]
+
+ // }
+ // comoMeChamo(pessoa)
+ // console.log(`eu me chamo $[pessoa.nome],mas pode me chamar de $[pessoa.apelido[0]], $[pessoa.apelido[1]],$[pessoa.apelido[2]]`)
+
+ // const novapessoa = {
+//      ...pessoa,
+ //     apelidos: ["olhodePolvo" ,"cabecao", "olhao" ]
+ // }
+
+ // function novapessoa(pessoa) {
+ //     ...pessoa
+ // [...apelidos]}
+
+ // exercicio 2
+
+//const estudante1 = {
+ //   nome: "fabio",
+ //   idade: 28,
+ //    profissao: "mecanico"}
+
+//const estudante2 = {
+    nome: "maria",
+    idade: 23,
+    profissao: "manicure"
+}
+   //  function estudantes(estudante1,estudante2) {
+   //      [...estudante1,]
+   //      [...estudante2 ]
+
+   //  }
+  //   estudantes(estudante1,estudante2)
+  //   const retorno = `${estudante1.nome} , ${estudantes1.nome.length}, ${estudante1.idade}, ${estudante1.profissao},${estudante1.profissao.length`}
+  //   const retorno1 = `${estudante2.nome}, ${estudante2.nome.length}, ${estudante2.idade}, ${estudante2.profissao}, ${estudante2.profissao.length`}
+
+  //  exercicio 3
+
+  //  const carrinho1 = []
+    
+   // const frutassacolao = {
+   //     frutas: ["banana", "maça", "manga"],
+  //      frutas1: ["morango", "pera","melancia"],
+  //      frutas3: ["melao" , "abacate","uva'"],
+  //      disponibilidade: true
+  //  }
+  //  function carrinho(frutas1) {
+   //     [...frutas1]
+  //  } carrinho = frutas1.push
+ // console.log(carrinho)
