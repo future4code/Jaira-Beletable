b216fff698d97de315e8d439a83bf23ba20a9034
Jaira-Beletable <jayjbeletable@gmail.com>
2021-09-30 22:10:12 -0300

    salvando o exercicio10

---
+++ funcoes-de-array/funcoes-de-array.js
@@ -0,0 +1,61 @@
+//console.log("ola joy") 
+ 
+// exercicio 1
+
+ //const pets = [
+  //   {nome: "lupin" , raca: "salsicha"},
+  //    {nome: "polly" , raca:"lhasa apso"},
+  //    {nome: "madame" , raca: "poodle"} ,
+  //    {nome: "quentinho" , raca:"salsicha"},
+  //    {nome: "fluffy" , raca:"poodle"},
+  //    {nome: "caramelo" , raca: "vira-lata"}
+  //    ]
+  //    const nomesDosPets = pets.map((item ,index, array) => {
+  //        return pets.item
+  //    })
+  //    const petsSalsicha = pets.filter((pets, indice, array) => {
+  //        return pets.tipo === "salsicha"
+   //   })
+   //   const descontoPoodle = pets.filter((pets, indice, array) => {
+  //     return pets.tipo === "poodle"
+
+   //   }) const imprimirDesconto = ("madame") => {
+  //        console.log("voce ganhou um desconto de 10% para tosar o/a " + ("madame"))
+  //       }const imprimirDesconto1 = ("fluffy") => {
+   //          console.log("voce ganhou um desconto de 10% para tosar o/a" + ("fluffy"))
+   //      }
+
+   //      exercicio 2
+
+         //const produtos = [
+          //   {nome: "alface lavada" , categoria: "hortifruti" , preco: 2.5}
+         //    {nome: "guarana21" , categoria: "bebidas" , preco: 7.8}
+         //    {nome: "veja multiuso" , categoria: "limpeza" , preco: 12.6}
+         //    {nome: "duzia de banana" , categoria: "hortifruti" , preco: 5.7}
+         //    {nome: "leite" , categoria: "bebidas" , preco: 2.99}
+         //    {nome: "candida" , categoria: "limpeza" , preco: 3.30}
+         //     {nome: "detergente ype", cate3goria: "limpeza", preco: 2.2}
+         //     {nome: "vinho tinto", categoria: "bebidas", preco: 55}
+         //     {nome: "berinjela kg", categoria: "hortifruti", preco: 8.99}
+          //    {nome: "sabao em po ype", categoria: "limpeza" , preco: 10.80}
+         //     ]
+
+     // const nomesCompras = produtos.map(produtos, indice, array) => {
+     //     return produtos.nome
+     // })
+     // const descontoDe5% = produtos.maps(produtos,categorias,preco) => {
+     //     return produtos  % 5
+     // })
+     // const somenteBebidas = produtos.filter(produtos,categorias,preco) =>{
+     //     return produtos.categorias === "bebidas"
+     // })
+     // const somenteProdutosYpe = produtos.filter(produtos,categorias,preco) => {
+     //     return produtos.nome === "ype"
+    //  })
+     // const produtosYpe = produtos.filter(produtos,categorias,preco) => {
+     //     return produtos.nome === "ype"
+     //     const produtosypePreco (precd) 
+      //    const produtosYpe1 = ("ype") => {
+     //         console.log("compre" + ("ype") + (preco))
+     //    }
+     // }
