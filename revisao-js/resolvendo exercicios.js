1bf6515a1a421e40fb356a3c57f889521874b015
Jaira-Beletable <jayjbeletable@gmail.com>
2021-10-07 21:11:36 -0300

    salvando exercicio 10

---
+++ revisao-js/resolvendo exercicios.js
@@ -0,0 +1,156 @@
+console.log("Olá")
+
+exercicio 1
+
+ function arrayInvertido() {
+     let array = [0,4,5,8,89,78]
+     return array.reverse()
+     } arrayInvertido()
+
+ exercicio 2
+
+ function arrayPares() {
+      const arrayP =  [2,5,6,34,54,21]
+     const arrays = arrayP.filter((arrayPares,indice,array) => {
+         return arrayPares.tipo % 2 === 0 * 2
+     }) arrayPares()
+
+     exercicio 3
+
+     function arraysPares() {
+         const arrayDeNumeros = [34,67,56,45,89]
+         const arrayDeNumeros1 = arrayDeNumeros.filter((arraysPares,indice,array) => [
+             return arraysPares.tipo % 2 === 0
+     })   arraysPares()
+
+     exercicio 4 
+
+     function maiorArray() {
+         const novoArray = [45,78,6,7,42,34]
+        const arrayMaior = maiorArray.filter((maiorArray,indice,array) => {
+        return maiorArray.tipo.lenghat >= [4] 
+        }) maiorArray()
+ 
+ exercicio 5
+
+ function arrayParametro( [4,5,7,8,56,4 ]) {
+  const elementosDoarray = arrayParametro.map((arrayParametro,indice,array) => {
+return arrayParametro.tipo.lenghat
+  }) arrayParametro()
+  
+ exercicio 6
+
+ function arraysBooleanos() {
+     const respostas = [false,false,true,true,true] 
+        return respostas
+        } arraysBooleanos()
+
+   exercicio 7
+
+   const retornarNumero = ( 4,5,6,8,7) => {
+       if (4,5,6,8,7) % 2 === 0{
+      return resultado  = (4,5,6,8,7) / 2
+       
+     
+      }
+   }
+   exercicio 8
+
+   function triangulos() {
+       const triangulo1 = [6,8,4]
+       const triangulo2 = [4,4,4] 
+       const triangulo3 = [4,4,5]
+       return resultadoDosTriangulos ("esse triangulo é escaleno" , + triangulo1 , + "esse triangulo é equilatero" + , triangulo2 + ,"esse triangulo é isosceles" , + triangulo)
+       } triangulos()
+   
+   exercicio 9
+
+function doisNumeros() {
+    const numeros = [5,10]
+     const maior = 5 > 10 
+     const divisivel = 5 / 10 % 2 
+     const diferenca = 5 ! 10
+     }
+
+ 
+ exercicio 10
+
+ function arraysmaiores() {
+      const arraysDafuncao = [67,78,45,23,40]
+      const arraysMaiores1 =  arraysmaiores.map((arraysmaiores,indice,array)) => {
+          return resultadoArrayMaiores  >= [4]
+         const arraysMenores = arrayMaiores.map((arraysmaiores,indice,array) => {
+             return resultadoAarraymenores <= [4]
+         }) arraysmaiores()
+ 
+ exercicio 11
+
+         function arraysnumeros2() {
+             const arraysOrdem = [34,67,45,32,12]
+            return arraysOrdem.lenghat < [4]
+            } arraysnumeros2()
+exercicio 12
+ function filmeFavorito() {
+        const objetoFilme = {nome:"o diabo veste prada"}
+      {ano: 2006 }
+      {diretor: "david frankel"}
+      {atores: " anne hathaway", "merly streep" , "emily blunt", "stanley tucci"}
+ }
+filmeFavorito()
+
+      exercicio 13
+function filmeAstrodev () {
+    [...objetoFilme]
+   return (" filme " + nome, + "de  ano" + ano , + " dirigido por" + diretor, + "estrelado pelos atores" + atores)
+        } 
+        filmeAstrodev()
+
+exercicio 14
+
+function retangulo(5,8) {
+const largura = 8
+const altura = 5
+const perimetro = ( 2 * 5 + 8)
+ const area = ( 5 * 8 )
+} retangulo()
+
+exercicio 15
+
+function pessoa() {
+    const quemE = {
+        nome: "eliana"
+        idade: 23
+        email: "lili@gmail.com"
+        endereco: " rua das rosas", 56
+    } const novaPessoa = quemE.replaceAll("eliana","anonimo")
+        return quemE
+    
+}pessoa()
+
+
+
+    
+
+ 
+
+ 
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+ 
+
+ ]
+ 
+ 
+ 
+
+ ]
+     
