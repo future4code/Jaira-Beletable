a6c9d7b60834b5ef0cab61fb8e9407224fe9ef7d
jaira-beletable <jayjbeletable@gmail.com>
2021-12-17 17:07:25 -0200

    enviando o pr

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react"
 import axios from "axios"
 import  styled from 'styled-components'
 
-class App extends React.Component() {
+export default class App extends React.Component() {
   ComponentDidMount() {
     this.criarPlaylist()
   }
   
   state={
-    buscarPorNome:"",
+    buscarPorNome:"" ,
     nomeDaMusica: "",
     nomeDoArtista: "",
     link:""
@@ -16,7 +16,21 @@ class App extends React.Component() {
   }
 criarPlaylist = async () => {
   try {
-    const response = await axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists', {
+    const response = await axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlist', {
+      headers: {
+        Authorization: 'jaira-beletable-joy'
+      }
+    });
+  
+console.log(response.data)
+}catch(error) {
+  console.log(error.response.data)
+}
+}
+}
+deletaPlaylist = async () => {
+  try {
+    const response = await axios.delete('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists${Id}',{
       headers: {
         Authorization: 'jaira-beletable-joy'
       }
@@ -26,33 +40,44 @@ console.log(response.data)
 }catch(error) {
   console.log(error.response.data)
 }
-}
-}
-onchangebuscarPorNome = (event) => {
+
+
+onChangebuscarPorNome = (event) => {
   this.setState({buscarPorNome: event.target.value})
 }
-onchangenomeDaMusica =  (event) => {
+onChangenomeDaMusica = (event) => {
   this.setState({nomeDaMusica: event.target.value})
 }
-onchangenomeDoArtista =  (event) => {
+onChangenomeDoArtista =  (event) => {
   this.setState({nomeDoArtista: event.target.value})
 }
-onchangelink = (event) => {
+onChangelink = (event) => {
   this.setState({link: event.target.value})
 
 }
-onclickbuscar = () => {
+onClickbuscar = () => {
   this.setState ({buscarPorNome: this.state.buscarPorNome})
 }
 
   render() ;{
+const renderPlaylist = this.state.buscarPorNome.map((playlist) => {
+  return <p>{this.buscarPorNome.nome}</p>
+});
+
   return (
     <div>
       <h2>labefy</h2>
+      <input placeholder="buscarplaylist" value={this.state.buscarPorNome} onChange={ this.onChangebuscarPorNome} />
+      <button onclick={this.onclickbuscar}>buscar</button>
+      <input placeholder="nomedamusica" value={this.state.nomeDaMusica}  onChange={this.onChangenomeDaMusica} />
+      <input placeholder="nomedoartista" value={this.state.nomeDoArtista}  onChange={this.onChangenomeDoArtista} />
+      <input placeholder="link" value={this.state.link} onChange={this.onChangelink} />
+      <button onClick={this.criarPlaylist}>adicionar playlist</button>
+      <button onClick={this.deletaPlaylist}>x</button>
+      <div>{renderPlaylist}</div>
     </div>
   );
   }
   
-  
+}
 
-export default App;
