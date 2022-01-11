7cb580f9e36b38f67b06e492cf47c5c8d9dc87a5
jaira-beletable <jayjbeletable@gmail.com>
2022-01-11 16:34:32 -0200

    pr-astromatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import axios from "axios"
+import  {useState,useEffect} from "react"
 
-function App() {
+
+
+
+ export function App() {
+   const [removerCard,setRemoverCard] = useState({});
+   const [matchCard,setMatchCard] = useState({});
+   const [id,setId] = useState(1)
+   const[cards,setCards] = useState({})
+   const[telaMatch,setTelaMatch] = useState({})
+   
+
+   const botaoRemover = () => {
+     setRemoverCard(removerCard = false)
+   }
+   const botaoMatch = () => {
+     setMatchCard(matchCard = true)
+   }
+   const botaoTelaMatch = () => {
+   setTelaMatch(telaMatch = {}) 
+   }
+   const pegarPerfil= () => {
+    axios
+      .get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/person${id}`)
+      .then((res) => setId(res.data));
+  };
+
+  useEffect(() => {
+    pegarPerfil();
+  }, [id]);
+
+  const escolherCard = () => {
+    axios
+      .get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/choose-person${cards}`)
+      .then((res) => setCards(res.data));
+  };
+
+  useEffect(() => {
+    escolherCard();
+  }, [cards]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>bem-vindo ao astroMatch</h2> 
+      <img src="coracao.png" alt="imagemCoracao"></img>
+      <button onClick={botaoTelaMatch}><img src="casal.png" alt="Casal"></img></button> 
+      <p>{pegarPerfil && pegarPerfil.name}</p>
+      <button onClick={botaoMatch}><img src="coracao.png" alt="Coracao"></img></button>
+      <p>{escolherCard?.escolherCard.name}</p> 
+      <button onClick={botaoRemover}><img src="excluir.png" alt="Excluir"></img></button> 
     </div>
   );
 }
